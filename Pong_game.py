6996586e029929ff1c5093250e6b0dcd59dd46d3
Jimmy Phan <phanjimmy02@gmail.com>
2026-03-15 16:37:23 -0400

    Paddles for players 1 & 2

--- Pong_game.py
+++ Pong_game.py
@@ -1,10 +1,26 @@
 import turtle 
 
-wn = turtle.Screen()
+wn = turtle.Screen() #creates a window for the game
 wn.title("Pong")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0) 
+
+paddle_a = turtle.Turtle() #creates the left paddle for player 1
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+paddle_b = turtle.Turtle() #creates the right paddle for player 2
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
 
 while True: #main game loop
     wn.update()
